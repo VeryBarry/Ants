6e5dbea347cae1959387531d3e6f97cc1ecd7f6f
VeryBarry <barry.a.daniels@gmail.com>
2016-11-08 10:27:05 -0500

    initial import

---
+++ src/AntsProj/Ant.java
@@ -0,0 +1,13 @@
+package AntsProj;
+
+/**
+ * Created by VeryBarry on 11/8/16.
+ */
+public class Ant {
+double x, y;
+
+    public Ant(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
---
+++ src/AntsProj/Controller.java
@@ -0,0 +1,4 @@
+package AntsProj;
+
+public class Controller {
+}
